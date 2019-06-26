d5541bf1d8b418bc3e93a5d402790185038ccb0a
jcyesc <jcyesc@192.168.1.7>
2019-06-26 07:25:23 -0700

    Update description of the WifiScan

--- context-hub/wifi_scan.h
+++ context-hub/wifi_scan.h
@@ -10,15 +10,24 @@
  *
  * - Network name (SSI), consisting of up to 32 ASCII characters.
  * - Network ID (BSSID), consisting of 6 bytes
- * - Signal Strength (RSSI), consists of a number ranging from 20 to 100.
+ * - Signal Strength (RSSI), consists of a number ranging from 20 to -100 in increments of 0.5.
+ *
+ * Note: Instead of storing RSSI as a float that usually takes 4 bytes (depending of architecture), the
+ * RSSI is stored as a uint8_t. The reason of this is because there are 240 different numbers between
+ * 20 and -100 that we are interested (20, 19.5, 19, 18.5, .... , -99, -99,5, -100) and we can represent
+ * each number as an index. For example:
+ *
+ * - The index 0 represents 20
+ * - The index 40 represents 0
+ * - The index 240 represents 100
+ *
  */
 typedef struct WifiScan {
-	char ssid[SSID_LENGTH]; // Network name (SSID), consisting of up to 32 ASCII characters
-	uint8_t bssid[NETWORK_ID_SIZE]; // Network ID (BSSID), consisting of 6 bytes
+	char ssid[SSID_LENGTH];
+	uint8_t bssid[NETWORK_ID_SIZE];
 	uint8_t rssi_index; // Represents the index of Signal strength (RSSI)(a number ranging from 20 to -100).
-						// The index 0 represents 20, the index 40 represents 0, the index 240 represents 100.
+						//
 } WifiScanContent;
-
 
 #endif /* WIFI_SCAN_H_ */
 
